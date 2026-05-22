53e8e0147e5fcd0f80a7a7fbf2f81033b84ab69f
illia <illyavolosenuk@gmail.com>
2026-05-22 16:46:24 +0200

    Add JWT auth, permissions and pagination

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -8,6 +10,7 @@ from .serializers import CategorySerializer
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
 
     @action(detail=True, methods=["get"])
     def count_tasks(self, request, pk=None):
